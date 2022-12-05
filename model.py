611ca281a707122489cc936be47afbf823be21d9
KoertSchreurs <koert.schreurs@profit4cloud.nl>
2022-12-05 12:33:45 +0100

    Added pF1 metric

--- model.py
+++ model.py
@@ -8,3 +8,18 @@ def create_model(inp_dim=(256, 256, 3)):
     outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
     model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
     return model
+
+
+def pfbeta_tf(labels, preds, beta=1):
+    preds = tf.clip_by_value(preds, 0, 1)
+    y_true_count = tf.reduce_sum(labels)
+    ctp = tf.reduce_sum(preds[labels == 1])
+    cfp = tf.reduce_sum(preds[labels == 0])
+    beta_squared = beta * beta
+    c_precision = ctp / (ctp + cfp)
+    c_recall = ctp / y_true_count
+    if (c_precision > 0 and c_recall > 0):
+        result = (1 + beta_squared) * (c_precision * c_recall) / (beta_squared * c_precision + c_recall)
+        return result
+    else:
+        return 0.0
--- train_model.py
+++ train_model.py
@@ -1,13 +1,13 @@
-from model import create_model
+from model import create_model, pfbeta_tf
 from train_val_split import get_train_val_generator
 
 model = create_model()
 
 train_gen, val_gen = get_train_val_generator()
-
+metrics = [pfbeta_tf]
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
-              metrics=['accuracy'])
+              metrics=metrics)
 print(model.summary())
 
 history = model.fit(train_gen, validation_data=val_gen, epochs=5)
