501cda5e336fbdd08d1ca5f8aaab9fe877ea3f96
KoertSchreurs <koert.schreurs@profit4cloud.nl>
2022-12-07 16:05:05 +0100

    Added code to build efficient net

--- src/models/model.py
+++ src/models/model.py
@@ -1,12 +1,28 @@
+import efficientnet.tfkeras as efn
 import tensorflow as tf
 
 
-def create_model(inp_dim=(256, 256, 3)):
+def build_test_model(inp_dim=(256, 256, 3)):
     inputs = tf.keras.layers.Input(inp_dim)
     x = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', strides=4)(inputs)
     x = tf.keras.layers.Flatten()(x)
     outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
     model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def build_efficient_net(model_name="EfficientNetB4",
+                        inp_dim=(256, 256),
+                        include_top=False):
+    base = getattr(efn, model_name)(input_shape=(*inp_dim, 3),
+                                    weights='imagenet',
+                                    include_top=include_top)
+    inp = base.inputs
+    x = base.output
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)  # use GAP to get pooling result form conv outputs
+    x = tf.keras.layers.Dense(32, activation='silu')(x)  # use activation to apply non-linearity
+    x = tf.keras.layers.Dense(1, activation='sigmoid')(x)  # use sigmoid to convert predictions to [0-1]
+    model = tf.keras.Model(inputs=inp, outputs=x)
     return model
 
 
@@ -18,7 +34,7 @@ def pfbeta_tf(labels, preds, beta=1):
     beta_squared = beta * beta
     c_precision = ctp / (ctp + cfp)
     c_recall = ctp / y_true_count
-    if (c_precision > 0 and c_recall > 0):
+    if c_precision > 0 and c_recall > 0:
         result = (1 + beta_squared) * (c_precision * c_recall) / (beta_squared * c_precision + c_recall)
         return result
     else:
