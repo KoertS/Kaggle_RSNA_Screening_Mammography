8a6c1389776ea30788c9287393191e5504caa607
KoertSchreurs <koert.schreurs@profit4cloud.nl>
2022-12-07 13:39:25 +0100

    Added preprocess_test_images.py

---
+++ src/data/preprocess_test_images.py
@@ -0,0 +1,8 @@
+from src.data.preprocessor import Preprocessor
+
+if __name__ == '__main__':
+    input_directory = '../../data/external/test_images/'
+    output_directory = '../../data/processed/test_images_processed/'
+    print(f'Preprocessing: {input_directory}')
+    preprocessor = Preprocessor(input_directory=input_directory, output_directory=output_directory, workers=32)
+    preprocessor.preprocess()
