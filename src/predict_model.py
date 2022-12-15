7a37b8ea251b1005d069a2867e138a1c57c68749
KoertSchreurs <koert.schreurs@profit4cloud.nl>
2022-12-15 12:01:07 +0100

    Added use of config.yaml in predict_model.py

--- src/predict_model.py
+++ src/predict_model.py
@@ -1,22 +1,31 @@
+import argparse
+
 import pandas as pd
 import tensorflow as tf
+import yaml
 
+from data.datagenerator import DataGenerator
 from models.model import pfbeta_tf
-from src.data.datagenerator import DataGenerator
 
-image_dir = '../data/processed/test_images_processed/'
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, nargs='?', default='../config/config.yaml', help='Path to the config file')
+parser.add_argument('--model', type=str, nargs='?', default='test_model.h5', help='Filename of model h5 file')
+
+args = parser.parse_args()
+
+with open(args.config) as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
 
 custom_metric = {"pfbeta_tf": pfbeta_tf}
-path_model = '../models/test_model.h5'
-model = tf.keras.models.load_model(path_model, custom_objects=custom_metric)
+model = tf.keras.models.load_model(config['data']['path_model'] + args.model, custom_objects=custom_metric)
 
-filename = '../data/raw/test.csv'
-df_test = pd.read_csv(filename)
+df_test = pd.read_csv(config['data']['test_frame'])
 patient_ids = df_test["patient_id"].unique()
 prediction_ids = df_test["prediction_id"].unique()
 
-test_gen = DataGenerator(dataframe=df_test, batch_size=1, path_images=image_dir)
+test_gen = DataGenerator(dataframe=df_test, batch_size=1, path_images=config['data']['test_images_processed_dir'])
+
 y_pred = model.predict(test_gen).T[0]
 df_submission = pd.DataFrame({'prediction_id': prediction_ids, 'cancer': y_pred})
 print(df_submission.head())
-df_submission.to_csv('../../data/output/submission.csv', index=False)
+df_submission.to_csv(config['data']['path_submission'], index=False)
