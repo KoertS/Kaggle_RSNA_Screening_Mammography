0444b42e2a16322830960c0bf63ba2aa50f24c50
KoertSchreurs <koert.schreurs@profit4cloud.nl>
2023-01-05 10:39:49 +0100

    Set project name in wandb to be same across different environments

--- src/train_model.py
+++ src/train_model.py
@@ -19,6 +19,7 @@ wandb.login(key=os.environ.get("WANDB_API_KEY"))
 wandb.init(project="Kaggle_RSNA_Screening_Mammography")
 wandb.config.update(config['hyperparams'])
 wandb_callback = wandb.keras.WandbCallback(log_weights=True)
+wandb.log({"environment": args.kaggle})
 
 model = create_model(config['hyperparams'])
 print(f'Training model: {model.name}')
